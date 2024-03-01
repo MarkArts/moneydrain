0af3f0187ec8887591b63eac52a51fc0f2e317c0
Mark Arts <marktarts93@gmail.com>
2024-03-01 15:32:41 +0100

    remove examples

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,10 @@
-import { formatEther, parseEther } from "viem";
 import hre from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
-
-  const lockedAmount = parseEther("0.001");
-
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const lock = await hre.viem.deployContract("Moneydrain", []);
 
   console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `deployed to ${lock.address}`
   );
 }
 
